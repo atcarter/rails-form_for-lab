84f53bafd931fad6758682534bb45149a2d5c40b
Antonio <atcarter005@gmail.com>
2021-07-08 19:40:49 -0500

    commit tree cb4822ba9d7650dc0f4c38d377947c918c01b0e8

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,34 @@
+class SchoolClassesController < ApplicationController
+
+  def index
+    @schoolclasses = SchoolClass.all
+  end
+  def new
+    @schoolclass = SchoolClass.new
+  end
+
+  def create
+    @schoolclass = SchoolClass.create(schoolclass_params)
+    redirect_to school_class_path(@schoolclass)
+  end
+
+  def show
+    @schoolclass = SchoolClass.find_by(params[:id])
+  end
+
+  def edit
+    @schoolclass = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @schoolclass = SchoolClass.find_by(params[:id])
+    @schoolclass.update(schoolclass_params)
+    redirect_to school_class_path(@schoolclass)
+  end
+
+  private
+
+  def schoolclass_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
+end
